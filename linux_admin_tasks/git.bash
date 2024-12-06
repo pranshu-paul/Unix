b883927fcc2bbc903ec6d117d3e69bbc702e835c
Pranshu Paul <paulpranshu@gmail.com>
2024-12-06 20:57:33 +0530

    Updated git.bash

--- linux_admin_tasks/git.bash
+++ linux_admin_tasks/git.bash
@@ -18,3 +18,33 @@ git push
 
 # Update the repo if the changes are made to another server.
 git pull
+
+# To check the current branch.
+git branch
+
+# Create & switch to a new branch.
+git switch -c dev
+
+# Switch back to prod.
+git switch prod
+
+# Switch to the dev branch.
+git switch dev
+
+# stage the files on the dev branch.
+git add <file1> <file2>
+
+# Commit the changes in the dev branch
+git commit -m "Changed the task one"
+
+# If the changes are successful. We have to push the changes in the prod branch.
+git switch prod
+
+# Megre the brances.
+git merge dev
+
+# If want to discard the changes from the branch merging.
+git reset --hard HEAD~1
+
+# To check the git log
+git log

ec30096fe4d4300cee89c0e4f24a5aa385dade52
Pranshu Paul <paulpranshu@gmail.com>
2024-12-07 19:41:43 +0530

    Deleted some C files

--- clang/post-c0.bash
+++
@@ -1,9 +0,0 @@
-
-# Install postgresql library.
-dnf -y install libpq-devel.x86_64
-
-# Change all connection authentication methods to md5.
-vim pg_hba.conf
-
-# To compile a C code with postgresql library.
-gcc -o connect connect.c -lpq

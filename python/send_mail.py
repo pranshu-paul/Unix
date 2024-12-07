97b392afe77bbc212c6709deb04ef8ee893766d7
Pranshu Paul <paulpranshu@gmail.com>
2024-12-07 20:43:33 +0530

    Added comments in send_email.py

--- python/send_mail.py
+++ python/send_mail.py
@@ -4,6 +4,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
+# Email details
 email = 'paulpranshu@outlook.com'
 recipient = 'paulpranshu@gmail.com'
 file_path = 'hr.sql'
@@ -12,25 +13,30 @@ subject = 'Test #7'
 password = ''
 message = "This is the body of your email."
 
+# Server configuration
 smtp_server = 'smtp.office365.com'
-
 smtp_port = 587
 
+# Initiate smtp connection
 smtp = smtplib.SMTP(smtp_server, smtp_port)
 smtp.ehlo()
 smtp.starttls()
 smtp.login(email, password)
 
+# Set email headers
 header = MIMEMultipart()
 header['From'] = name
 header['To'] = recipient
 header['Subject'] = subject
 
+# Provide the MIME type of the message
 header.attach(MIMEText(message, 'plain'))
 
+# Open the file in the read-only binary mode
 filename = file_path
 attachment = open(filename, 'rb')
 
+# Provide the MIME type of the attachement
 part = MIMEBase('application', 'octet-stream')
 part.set_payload(attachment.read())
 encoders.encode_base64(part)
@@ -38,6 +44,6 @@ part.add_header('Content-Disposition', f'attachment; filename= {filename}')
 header.attach(part)
 
 text = header.as_string()
-smtp.sendmail(email, recipient, text)
+smtp.sendmail(email, recipient, text)           # Send the mail
 
 smtp.quit()
